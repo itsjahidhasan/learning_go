0554f8155c2ffba2f97d9ebc4a012e20d2cda954
Md Hasan <md.hasan@technonext.com>
2025-01-30 18:49:27 +0600

    x

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+type Subjective struct {
+	name string
+	grade int
+}
+
+type TotalResult struct {
+	name string
+	value float64
+	english Subjective
+}
+
+func main(){
+
+	// var result1 TotalResult;
+	// result1.name = "myResult";
+	// result1.value = 10.5;
+	// result1.english.name = "English";
+	// fmt.Println(result1)
+
+    
+
+
+	
+}
+
+func printArray(s [][]string){
+	for i:=0;i<len(s);i++{
+		
+	}
+}
